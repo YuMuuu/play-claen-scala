8cc8aa06a6f4ec6ddff2cad39709d99f6d26f0eb
yu miura <curelemonade15@gmail.com>
2026-09-11 02:13:42 +0900

    docs: UseCaseの型設計案をコメントで追記

--- modules/usecase/src/main/scala/dev/yumuuu/playclean/usecase/UseCase.scala
+++ modules/usecase/src/main/scala/dev/yumuuu/playclean/usecase/UseCase.scala
@@ -2,6 +2,18 @@ package dev.yumuuu.playclean.usecase
 
 import cats.effect.IO
 
+/**
+ * 以下のようにした方がより厳密に型を定義できる？？
+ * type UserResult[A] = Either[UserApplicationError, A]
+ *
+ * trait UseCase[Input, M[_]: Functor, Model, View] {
+ *   protected def execute(input: Input): IO[M[Model]]
+ *   protected def toView(model: Model): View
+ *
+ *   final def handle(input: Input): IO[M[View]] = execute(input).map(_.map(toView))
+ * }
+ */
+
 trait UseCase[Input, Output] {
   def handle(input: Input): IO[Output]
 }
